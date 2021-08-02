93ea9b1e0e55cd5b33bcd334aa3ab704d6999c27
taras0024 <74822918+taras0024@users.noreply.github.com>
2021-08-02 15:57:35 +0300

    pagination

--- movies/service.py
+++ movies/service.py
@@ -1,4 +1,6 @@
 from django_filters import rest_framework as filters
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 from movies.models import Movie
 
@@ -25,3 +27,17 @@ class MovieFilter(filters.FilterSet):
     class Meta:
         model = Movie
         fields = ['genres', 'year']
+
+class PaginationMovie(PageNumberPagination):
+    page_size = 2
+    max_page_size = 1000
+
+    def get_paginated_response(self, data):
+        return Response({
+            'links': {
+                'next': self.get_next_link(),
+                'previous': self.get_previous_link(),
+            },
+            'count': self.page.paginator.count,
+            'result': data,
+        })
